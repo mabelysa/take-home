7b769536607b738b98a8b72df87de54fa7f8fef1
Mabelys Arias <MabelysA@gmail.com>
2021-08-04 13:58:40 -0400

    initial commit

---
+++ src/components/CSVimport.vue
@@ -0,0 +1,19 @@
+<template>
+  <vue-csv-import
+    v-model="csv"
+    :fields="{ name: { required: false, label: 'Name' }, price: { required: true, label: 'Price' } }"
+  >
+    <vue-csv-toggle-headers></vue-csv-toggle-headers>
+    <vue-csv-errors></vue-csv-errors>
+    <vue-csv-input></vue-csv-input>
+    <vue-csv-map></vue-csv-map>
+  </vue-csv-import>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
